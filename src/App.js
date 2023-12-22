7c51fe2eb8b6bbf9be5acc91603a0ee531d0887f
gofordivya <gofordivya@gmail.com>
2023-12-22 15:12:18 +0100

    implemented axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import UserProfile from "./components/UserProfile";
 import UserRepository from "./components/UserRepository";
 
 function App() {
+  const [userData, setUserData] = useState({});
+
+  const [userRepository, setUserRespository] = useState([]);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const clientId = "5f72d84a424555f5624c";
+        const clientSecret = "614b5ca8f28f09b0ba4bf5ea8dfc430d32fd08a6";
+        const username = "gofordivya";
+        const endpoint = `https://api.github.com/users/${username}?client_id=${clientId}&client_secret=${clientSecret}&sort=created`;
+
+        const userResponse = await axios.get(endpoint);
+
+        const responsitoryResponse = await axios.get(
+          `https://api.github.com/users/${username}/repos`
+        );
+
+        setUserData(userResponse.data);
+        setUserRespository(responsitoryResponse.data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
   return (
     <div className="container mt-4">
       <div className="col-md-12">
-        <UserProfile></UserProfile>
+        <UserProfile userData={userData}></UserProfile>
       </div>
-      <div className="col-md-12">
-        <UserRepository></UserRepository>
+      <div className="col-md-12 mt-4">
+        <UserRepository userRepository={userRepository}></UserRepository>
       </div>
     </div>
   );
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,22 +1,23 @@
-function UserProfile() {
+function UserProfile({ userData }) {
+  const { login, avatar_url, name, location, email } = userData;
   return (
     <div className="row">
       <div className="col-md-4">
-        <img src="" alt="User" className="img-thumbnail" />
+        <img src={avatar_url} alt="User" className="img-thumbnail" />
       </div>
       <div className="col-md-8">
         <ul className="list-group">
           <li className="list-group-item">
-            <b>Fullname:</b> Divya
+            <b>Fullname:</b> {name}
           </li>
           <li className="list-group-item">
-            <b>Username:</b> Divya
+            <b>Username:</b> {login}
           </li>
           <li className="list-group-item">
-            <b>Location:</b> Divya
+            <b>Location:</b> {location}
           </li>
           <li className="list-group-item">
-            <b>Email Address:</b> Divya
+            <b>Email Address:</b> {email}
           </li>
         </ul>
       </div>
