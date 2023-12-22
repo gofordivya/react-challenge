13757e9d4aef6f42053b4f10d71d17a2804d824d
gofordivya <gofordivya@gmail.com>
2023-12-22 16:34:27 +0100

    added textbox feild

--- src/App.js
+++ src/App.js
@@ -6,35 +6,56 @@ import UserRepository from "./components/UserRepository";
 
 function App() {
   const [userData, setUserData] = useState({});
-
   const [userRepository, setUserRespository] = useState([]);
+  const [inputUsername, setInputUsername] = useState("");
+
+  const fetchUserData = async (inputUsername) => {
+    try {
+      const clientId = "5f72d84a424555f5624c";
+      const clientSecret = "614b5ca8f28f09b0ba4bf5ea8dfc430d32fd08a6";
+      const username = inputUsername ? inputUsername : "gofordivya";
+      const endpoint = `https://api.github.com/users/${username}?client_id=${clientId}&client_secret=${clientSecret}&sort=created`;
+
+      const userResponse = await axios.get(endpoint);
+
+      const responsitoryResponse = await axios.get(
+        `https://api.github.com/users/${username}/repos`
+      );
+
+      setUserData(userResponse.data);
+      setUserRespository(responsitoryResponse.data);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const clientId = "5f72d84a424555f5624c";
-        const clientSecret = "614b5ca8f28f09b0ba4bf5ea8dfc430d32fd08a6";
-        const username = "gofordivya";
-        const endpoint = `https://api.github.com/users/${username}?client_id=${clientId}&client_secret=${clientSecret}&sort=created`;
-
-        const userResponse = await axios.get(endpoint);
-
-        const responsitoryResponse = await axios.get(
-          `https://api.github.com/users/${username}/repos`
-        );
-
-        setUserData(userResponse.data);
-        setUserRespository(responsitoryResponse.data);
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      }
-    };
-
-    fetchUserData();
+    fetchUserData(inputUsername);
   }, []);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    fetchUserData(inputUsername);
+  };
 
   return (
     <div className="container mt-4">
+      <div className="col-md-4 mb-4 d-flex align-items-end">
+        <form onSubmit={handleFormSubmit}>
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              value={inputUsername}
+              placeholder="enter github username"
+              onChange={(e) => setInputUsername(e.target.value)}
+            />
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
       <div className="col-md-12">
         <UserProfile userData={userData}></UserProfile>
       </div>
